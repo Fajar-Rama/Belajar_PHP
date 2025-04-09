272b4b23dac2b9c71e21edbbdd2a9ffc2f38b426
Fajar-Rama <muhamadramadan780@gmail.com>
2025-04-09 07:46:35 +0700

    Add files via upload

--- variable.php
+++ variable.php
@@ -1,3 +1,12 @@
 <?php
 echo "hello_everyone";
 echo "My_House_is" . $color = "red";
+echo "welcome_to_my_website";//
+$x = 10;
+$y = John;
+$z = 50;
+$age = 23;
+$txt = "school_um";
+echo "I like .$txt";
+echo $x * $z;
+
